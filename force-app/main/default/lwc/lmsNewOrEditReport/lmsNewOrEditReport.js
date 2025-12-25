db6c7e9d855a8c56e3b4446b1fb6c81ade1786c0
leelasairam <saikodavali0948@gmail.com>
2025-12-25 11:10:03 +0530

    added custom logic

--- force-app/main/default/lwc/lmsNewOrEditReport/lmsNewOrEditReport.js
+++ force-app/main/default/lwc/lmsNewOrEditReport/lmsNewOrEditReport.js
@@ -20,6 +20,11 @@ export default class NewOrEditReport extends LightningModal {
         { label: 'Not Equal', value: '!=' },
         { label: 'Contains', value: 'LIKE' },
     ];
+    @track fieldDataTypeMap = new Map();
+    sortOrder = [
+        { label: 'Acending', value: 'ASC' },
+        { label: 'Decending', value: 'DESC' },
+    ]
 
     connectedCallback(){
         if(this.filterCounterList == null || this.filterCounterList == ''){
@@ -40,7 +45,8 @@ export default class NewOrEditReport extends LightningModal {
         getFields({objectApiName:obj})
         .then(result=>{
             for (const [fieldAPIName, fieldLabel] of Object.entries(result)) {
-                fields.push({label:fieldLabel,value:fieldAPIName});
+                fields.push({label:fieldLabel[0],value:fieldAPIName});
+                this.fieldDataTypeMap.set(fieldAPIName,fieldLabel[1]);//adding data type of field
             }
             this.objFields = fields;
         })
@@ -60,6 +66,9 @@ export default class NewOrEditReport extends LightningModal {
         const value = event.target.value;
         this.filterCounterList = this.filterCounterList.map((row)=>{
             if(row.Id == Id){
+                if(inputType==='Field'){
+                    return { ...row, [inputType]: value,dataType : this.fieldDataTypeMap.get(value) };
+                }
                 return { ...row, [inputType]: value };
             }
             return row;
@@ -71,5 +80,47 @@ export default class NewOrEditReport extends LightningModal {
         const Id = event.target.dataset.id;
         this.filterCounterList = this.filterCounterList.filter(i=>i.Id!=Id);
         console.log(JSON.stringify(this.filterCounterList));
+    }
+
+    subsituteCustomLogic(){
+        const container = this.refs.customFilterOrderby;
+        let logic = container.querySelector('.cLogic').value; //1 AND (2 OR 3)
+        const sortbyField = container.querySelector('.sortBy').value;
+        const sortOrder = container.querySelector('.sortOrder').value;
+        console.log(logic)
+        const logicStrArray = logic.match(/\d+/g) || [];
+        const logicNumArray = logicStrArray.map(n=>(Number(n)));
+        logicNumArray.forEach(i=>{
+            const filterRowStr = this.getfilterRowStr(i);
+            // replace ONLY the exact number (not partial matches)
+            const regex = new RegExp(`\\b${i}\\b`, 'g');
+            logic = logic.replace(regex,filterRowStr);
+        })
+        console.log(logic);
+    }
+
+    getfilterRowStr(Id){
+        const filterRow = this.filterCounterList.find(row=>row.Id===Id);
+        if (!filterRow){
+            return '';
+        } 
+        let filterStr = '';
+        let value = this.filterCounterList?.Value;
+
+        switch(this.filterCounterList?.dataType){
+            case 'BOOLEAN':
+                value = value.toLowerCase();
+                break;
+            case 'DATETIME':
+            case 'DATE':
+        }
+
+        if(filterRow.Operation == 'LIKE'){
+            filterStr = `${filterRow.Field} ${filterRow.Operation} '%${filterRow.Value}%'`;
+        }
+        else{
+            filterStr = `${filterRow.Field} ${filterRow.Operation} ${filterRow.Value}`;
+        }
+        return filterStr;
     }
 }
